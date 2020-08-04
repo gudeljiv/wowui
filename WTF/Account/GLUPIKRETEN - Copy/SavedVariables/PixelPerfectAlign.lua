698dc07d87f4ac3194d8f425eae33815f55ce28c
Ivan Gudelj <ivan@borming.hr>
2020-08-04 18:33:10 +0200

    .

--- WTF/Account/GLUPIKRETEN - Copy/SavedVariables/PixelPerfectAlign.lua
+++
@@ -1,5 +0,0 @@
-
-pixelPerfectAlignSaved = {
-	["addonVersion"] = "v1.07.01-classic",
-	["addonHash"] = "c102190",
-}
--- WTF/Account/GLUPIKRETEN - Copy/SavedVariables/TitanClassicGold.lua
+++
@@ -1,8 +0,0 @@
-
-GoldSave = {
-	["Paydo_Dreadmist::Horde"] = {
-		["show"] = true,
-		["name"] = "Paydo",
-		["gold"] = 181,
-	},
-}
