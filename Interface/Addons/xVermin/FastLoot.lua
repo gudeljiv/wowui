383f07af42fd48f8d48195a32fe8ca1d038788b3
Ivan Gudelj <ivan@borming.hr>
2020-08-24 13:39:59 +0200

    .

--- Interface/Addons/xVermin/FastLoot.lua
+++ Interface/Addons/xVermin/FastLoot.lua
@@ -1,14 +1,14 @@
 local _, xVermin = ...
 
-local tDelay = 0
+local delay = 0
 local function FastLoot()
-	if GetTime() - tDelay >= 0.3 then
-		tDelay = GetTime()
+	if GetTime() - delay >= 0.3 then
+		delay = GetTime()
 		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
 			for i = GetNumLootItems(), 1, -1 do
 				LootSlot(i)
 			end
-			tDelay = GetTime()
+			delay = GetTime()
 		end
 	end
 end
