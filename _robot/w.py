9a7ee6e83ba42f81d3a2d77d03305f4957ed6723
Ivan Gudelj <ivan@borming.hr>
2022-01-07 17:52:32 +0100

    .

--- _robot/w.py
+++ _robot/w.py
@@ -14,13 +14,13 @@ x = 1280
 y = 860
 
 skills = [
-    {"name": "rend", "key": "5", "r": 79, "g": 2, "b": 0, "rgb": "(79,2,0)"},
-    {"name": "battle shout", "key": "f4", "r": 193, "g": 115, "b": 110, "rgb": "(193,115,110)"},
-    {"name": "overpower", "key": "4", "r": 89, "g": 97, "b": 107, "rgb": "(89,97,107)"},
-    {"name": "heroic strike", "key": "2", "r": 153, "g": 150, "b": 153, "rgb": "(153,150,153)", "aoe": False},
-    {"name": "cleave", "key": "3", "r": 153, "g": 150, "b": 153, "rgb": "(153,150,153)", "aoe": True},
-    {"name": "sunder armor", "key": "g", "r": 189, "g": 126, "b": 116, "rgb": "(189,126,116)"},
-    {"name": "revenge", "key": "1", "r": 205, "g": 195, "b": 195, "rgb": "(205,195,195)"}
+    {"name": "rend", "key": "5", "r": 79, "g": 2, "b": 0, "rgb": (79, 2, 0)},
+    {"name": "battle shout", "key": "f4", "r": 193, "g": 115, "b": 110, "rgb": (193, 115, 110)},
+    {"name": "overpower", "key": "4", "r": 89, "g": 97, "b": 107, "rgb": (89, 97, 107)},
+    {"name": "heroic strike", "key": "2", "r": 153, "g": 150, "b": 153, "rgb": (153, 150, 153), "aoe": False},
+    {"name": "cleave", "key": "3", "r": 153, "g": 150, "b": 153, "rgb": (153, 150, 153), "aoe": True},
+    {"name": "sunder armor", "key": "g", "r": 189, "g": 126, "b": 116, "rgb": (189, 126, 116)},
+    {"name": "revenge", "key": "1", "r": 205, "g": 195, "b": 195, "rgb": (205, 195, 195)}
 ]
 
 
@@ -55,6 +55,7 @@ with keyboard.Listener(on_press=on_press) as listener:
                 continue
 
             for skill in skills:
+                # print(pyautogui.pixel(x, y), skill["rgb"], pyautogui.pixel(x, y) == skill["rgb"])
                 if pyautogui.pixel(x, y) == skill["rgb"]:
                     # if pyautogui.pixel(x, y)[0] == skill["r"] and pyautogui.pixel(x, y)[1] == skill["g"] and pyautogui.pixel(x, y)[2] == skill["b"]:
                     if "aoe" in skill and skill["aoe"] == aoe:
