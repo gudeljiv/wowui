608275cbc690973c4c6964665821512259986f1e
Ivan Gudelj <ivan@borming.hr>
2022-05-26 10:23:33 +0200

    .

--- _tbc/Interface/Addons/CharacterNotes/Locale/esMX.lua
+++
@@ -1,5 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("CharacterNotes", "esMX", false)
-
-if not L then return end
-
-
--- _tbc/Interface/Addons/CharacterNotes/Locale/frFR.lua
+++
@@ -1,5 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("CharacterNotes", "frFR", false)
-
-if not L then return end
-
-
