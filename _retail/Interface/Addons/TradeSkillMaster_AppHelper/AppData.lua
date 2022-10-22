eb27518f87fa55bcf2a3f4fc0837d5b52c5ad234
Ivan Gudelj <ivan@borming.hr>
2022-10-22 18:07:12 +0200

    .

--- _retail/Interface/Addons/TradeSkillMaster_AppHelper/AppData.lua
+++ _retail/Interface/Addons/TradeSkillMaster_AppHelper/AppData.lua
@@ -1,1 +1,1 @@
-select(2, ...).LoadData("APP_INFO","Global",[[return {version=411,lastSync=1666454045,message={id=0,msg=""},news={}}]]) --<APP_INFO,Global,1666454045>
+select(2, ...).LoadData("APP_INFO","Global",[[return {version=411,lastSync=1666454704,message={id=0,msg=""},news={}}]]) --<APP_INFO,Global,1666454704>
