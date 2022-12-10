38c77ecd1f62469787488e8d2c26f57f431819e4
Ivan Gudelj <ivan@borming.hr>
2022-12-10 11:02:50 +0100

    .

--- _wotlk/Interface/Addons/xVermin/Addons/RXPGuide.lua
+++ _wotlk/Interface/Addons/xVermin/Addons/RXPGuide.lua
@@ -9,21 +9,21 @@ xVermin.CheckIfLoadedWithTimer(
 
 		RXPFrame:ClearAllPoints()
 		RXPFrame:SetPoint('BOTTOMRIGHT', 'ChatFrame3', 'BOTTOMLEFT', 0, -10)
-	end
-)
 
-RXPItemFrame:HookScript(
-	'OnUpdate',
-	function(self)
-		if InCombatLockdown() then
-			return
-		end
+		RXPItemFrame:HookScript(
+			'OnUpdate',
+			function(self)
+				if InCombatLockdown() then
+					return
+				end
 
-		for i = 1, 10 do
-			ib = _G['RXPItemFrameButton' .. i]
-			if ib then
-				ib:CreateBeautyBorder(6)
+				for i = 1, 10 do
+					ib = _G['RXPItemFrameButton' .. i]
+					if ib then
+						ib:CreateBeautyBorder(6)
+					end
+				end
 			end
-		end
+		)
 	end
 )
