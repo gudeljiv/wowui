74a930aeaac50d49745ed77e75cb6d1e1b118154
Ivan Gudelj <ivan@borming.hr>
2023-10-23 09:11:25 +0200

    .

--- _classic/Interface/Addons/AtlasLootClassic_Crafting/data-wrath.lua
+++ _classic/Interface/Addons/AtlasLootClassic_Crafting/data-wrath.lua
@@ -422,9 +422,14 @@ data["EnchantingWrath"] = {
 				{ 8, 44582 },	-- Enchant Cloak - Minor Power
 				{ 9, 60609 },	-- Enchant Cloak - Speed
 
-				{ 16, 60609 },	-- Enchant Cloak - Speed
-				{ 17, 44582 },	-- Enchant Cloak - Spell Piercing
-				{ 18, 60663 },	-- Enchant Cloak - Major Agility
+				{ 20, 44596 },	-- Enchant Cloak - Superior Arcane Resistance
+				{ 19, 44556 },	-- Enchant Cloak - Superior Fire Resistance
+				{ 16, 44483 },	-- Enchant Cloak - Superior Frost Resistance
+				{ 17, 44494 },	-- Enchant Cloak - Superior Nature Resistance
+				{ 18, 44590 },	-- Enchant Cloak - Superior Shadow Resistance
+
+
+
 			}
 		},
 		{
@@ -1001,11 +1006,19 @@ data["LeatherworkingWrath"] = {
 				{ 6, 50966 },	-- Nerubian Leg Armor
 				{ 8, 50963 },	-- Heavy Borean Armor Kit
 
-				{ 10, 57683 },	-- Fur Lining - Attack Power
-				{ 11, 57691 },	-- Fur Lining - Spell Power
+				{ 10, 60584 },	-- Nerubian Leg Reinforcements
+				{ 11, 60583 },	-- Jormungar Leg Reinforcements
 
-				{ 25, 60584 },	-- Nerubian Leg Reinforcements
-				{ 26, 60583 },	-- Jormungar Leg Reinforcements
+				{ 16, 57691 },	-- Fur Lining - Spell Power
+				{ 17, 57690 },	-- Fur Lining - Stamina
+				{ 18, 57683 },	-- Fur Lining - Attack Power
+
+				{ 20, 57701 },	-- Fur Lining - Arcane Resist
+				{ 21, 57692 },	-- Fur Lining - Fire Resist
+				{ 22, 57694 },	-- Fur Lining - Frost Resist
+				{ 23, 57699 },	-- Fur Lining - Nature Resist
+				{ 24, 57696 },	-- Fur Lining - Shadow Resist
+
 			},
 		},
 		{
@@ -1228,7 +1241,7 @@ data["JewelcraftingWrath"] = {
 			[NORMAL_DIFF] = {
 				{ 1, 66498 },	-- Sparkling Majestic Zircon
 				{ 2, 66497 },	-- Solid Majestic Zircon
-				{ 3, 66498 },	-- Lustrous Majestic Zircon
+				{ 3, 66500 },	-- Lustrous Majestic Zircon
 				{ 4, 66499 },	-- Stormy Majestic Zircon
 				{ 6, 53953 },	-- Sparkling Sky Sapphire
 				{ 7, 53952 },	-- Solid Sky Sapphire
