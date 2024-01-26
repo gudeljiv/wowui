fd95b2832f920748ada9fb91bbec1c88d867b04d
Ivan Gudelj <gudeljiv@gmail.com>
2024-01-26 06:42:52 +0100

    .

---
+++ _classic/WTF/Account/GLUPIKRETEN/Lone Wolf/Dopedot/SavedVariables/HandyNotes_Runes.lua
@@ -0,0 +1,16 @@
+
+HandyNotes_RunesDB = {
+	["profiles"] = {
+		["Default"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Dopedot - Lone Wolf"] = "Default",
+	},
+	["runesKnown"] = {
+		["WARLOCK"] = {
+			["spells"] = {
+			},
+		},
+	},
+}
