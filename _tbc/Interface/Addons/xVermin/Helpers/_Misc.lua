93d0e627e9cee4c92b21df005be71ccb05bf37d5
Ivan Gudelj <gudeljiv@gmail.com>
2026-02-28 15:19:38 +0100

    .

--- _tbc/Interface/Addons/xVermin/Helpers/_Misc.lua
+++ _tbc/Interface/Addons/xVermin/Helpers/_Misc.lua
@@ -1,6 +1,9 @@
 local _, xVermin = ...
 
+xVermin.CheckIfLoadedWithTimer("PlayerCastingBarFrame.timer", function()
 
-PlayerCastingBarFrame.timer:ClearAllPoints()
-PlayerCastingBarFrame.timer:SetScale(1.2)
-PlayerCastingBarFrame.timer:SetPoint("RIGHT", 34, 2)
+	PlayerCastingBarFrame.timer:ClearAllPoints()
+	PlayerCastingBarFrame.timer:SetScale(1.2)
+	PlayerCastingBarFrame.timer:SetPoint("RIGHT", 34, 2)
+
+end, 1, 120)
