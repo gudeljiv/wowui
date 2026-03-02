e43bf0cab7cfd57ac903b9579eb814e9ce125513
Ivan Gudelj <gudeljiv@gmail.com>
2026-03-02 19:08:08 +0100

    .

---
+++ fix_linked_folders.cmd
@@ -0,0 +1,23 @@
+rmdir "C:\Games\World of Warcraft\_anniversary_\Fonts"
+rmdir "C:\Games\World of Warcraft\_anniversary_\Interface"
+rmdir "C:\Games\World of Warcraft\_anniversary_\WTF"
+rmdir "C:\Games\World of Warcraft\_classic_\Fonts"
+rmdir "C:\Games\World of Warcraft\_classic_\Interface"
+rmdir "C:\Games\World of Warcraft\_classic_\WTF"
+rmdir "C:\Games\World of Warcraft\_classic_era_\Fonts"
+rmdir "C:\Games\World of Warcraft\_classic_era_\Interface"
+rmdir "C:\Games\World of Warcraft\_classic_era_\WTF"
+
+
+
+mklink /D "C:\Games\World of Warcraft\_anniversary_\Fonts" "C:\Games\wowui\_tbc\Fonts"
+mklink /D "C:\Games\World of Warcraft\_anniversary_\Interface" "C:\Games\wowui\_tbc\Interface"
+mklink /D "C:\Games\World of Warcraft\_anniversary_\WTF" "C:\Games\wowui\_tbc\WTF"
+
+mklink /D "C:\Games\World of Warcraft\_classic_\Fonts" "C:\Games\wowui\_wotlk\Fonts"
+mklink /D "C:\Games\World of Warcraft\_classic_\Interface" "C:\Games\wowui\_wotlk\Interface"
+mklink /D "C:\Games\World of Warcraft\_classic_\WTF" "C:\Games\wowui\_wotlk\WTF"
+
+mklink /D "C:\Games\World of Warcraft\_classic_era_\Fonts" "C:\Games\wowui\_classic\Fonts"
+mklink /D "C:\Games\World of Warcraft\_classic_era_\Interface" "C:\Games\wowui\_classic\Interface"
+mklink /D "C:\Games\World of Warcraft\_classic_era_\WTF" "C:\Games\wowui\_classic\WTF"
